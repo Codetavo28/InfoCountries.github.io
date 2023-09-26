a9fe84761ae601de46064ac362510821caeded54
unknown <Codetavo28>
2023-09-26 19:47:36 -0400

    responsive

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-function getCountry(done) {
+const  results = fetch("https://restcountries.com/v3.1/all");
 
-    const  results = fetch("https://restcountries.com/v3.1/all");
+function getCountry(done) {
 
     results
     .then(response => response.json())
@@ -13,17 +13,26 @@ getCountry(data =>{
     console.log(data);
     const div1 = document.querySelector('#countries-div')
     data.forEach(country => {
-        
+        let population = (parseInt(country.population)).toLocaleString();
         const div2 = document.createElement('div')
         div2.innerHTML=
         `<img class="flags" src=${country.flags.svg} alt="">
         <h2 class="names">${country.name.common}</h2>
         <p class="data">Capital: ${country.capital}</p>
-        <p class="data">Population: ${country.population}</p>
+        <p class="data">Population: ${population}</p>
         <p class="data">Region: ${country.region}</p>
         `;
         div2.classList.add('countries')
         div1.append(div2)
     });
 });
+
+getCountry(data =>{
+    const countryInput = document.querySelector('#main-input')
+    data.forEach()
+
+});
+
+
+
 
